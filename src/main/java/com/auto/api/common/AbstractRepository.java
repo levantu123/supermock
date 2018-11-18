1b7802c2eeda4a1efa27943c71f71a1fff742758
Le tu <levantu.13139@gmail.com>
2018-11-18 15:46:04 +0700

    fix issues

--- src/main/java/com/auto/api/common/AbstractRepository.java
+++ src/main/java/com/auto/api/common/AbstractRepository.java
@@ -2,5 +2,7 @@ package com.auto.api.common;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface AbstractRepository<O> extends MongoRepository<O, String>{
+import com.auto.api.model.Request;
+
+public interface AbstractRepository<O extends Request> extends MongoRepository<O, String>{
 }
